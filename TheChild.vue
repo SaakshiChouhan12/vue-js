a00205d8274cf10d740e8f24abbe15a58cc5e318
SaakshiChouhan12 <91133730+SaakshiChouhan12@users.noreply.github.com>
2023-01-21 13:27:21 +0530

    Add files via upload

---
+++ TheChild.vue
@@ -0,0 +1,20 @@
+<template>
+
+<h2> {{name}}</h2>
+<h2> {{user.name}}</h2>
+<button v-on:click="getData()" >Call function</button>
+</template>
+
+<script>
+
+export default {
+ name: 'TheChild' ,
+ props:{
+     name:String,
+     user: Object,
+     getData: Function
+ }
+ 
+}
+
+</script>
