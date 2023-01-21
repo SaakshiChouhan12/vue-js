6d5df8ed258470822766924f0f05dbc08957e14a
SaakshiChouhan12 <91133730+SaakshiChouhan12@users.noreply.github.com>
2023-01-21 13:38:15 +0530

    Add files via upload

---
+++ vue component/MyHome.vue
@@ -0,0 +1,37 @@
+<template>
+
+<h1> pass data to child component from main-MyHome </h1>
+<TheChild  name="Vue Js" :user="user" :getData= "getData"  />
+</template>
+
+
+
+<script>
+import TheChild from './TheChild.vue'
+ export default{
+ name: 'MyHome',
+  components:{
+     TheChild
+  },
+  data(){
+ return{
+
+     user:{
+        name: 'Saakshi',
+        email: 'Saakshichouhan21@gmail.com'
+     }
+   }
+ },
+ methods: {
+   getData() {
+   console.warn("function is called")
+   alert("hello")
+   }
+
+ }
+
+
+ }
+</script>
+
+
