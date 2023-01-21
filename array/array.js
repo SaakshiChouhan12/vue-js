50b61e2204353124e2eea6ac29f60fe35a73f26d
SaakshiChouhan12 <91133730+SaakshiChouhan12@users.noreply.github.com>
2023-01-21 13:41:05 +0530

    Add files via upload

---
+++ array/array.js
@@ -0,0 +1,32 @@
+
+//remove last element of array
+const arr= ['A', 'B', 'C', 'D'];
+
+const eleLast = arr.pop();
+console.log(eleLast); 
+
+// push element in array
+arr.push('E' , 'F');
+console.log(arr);
+
+// add index in front of element
+for (var i = 0; i < arr.length; i++)
+    { console.log(arr[i]+ '('+ i +')'); }
+
+// mystring
+
+myString= arr.join('-');
+
+//eagle
+   const eagle = ["stork", "raven", "eagle", "ostrich"];
+let index = eagle.indexOf("raven");
+console.log(index);
+
+
+const startsWithN = eagle.filter((eagles) => eagles.startsWith("e"));
+
+console.log(startsWithN);
+
+
+  let eBirds = eagle.filter(firstN());
+ console.log(eBirds);
